46ea6587a340ecc269e903f6dd92663f568d0bd4
Gianmaria Schinezos <gnmr@Gianmarias-Laptop.local>
2025-06-18 13:16:02 +0200

    fix use client

--- src/app/(frontend)/_components/ImageGallery.tsx
+++ src/app/(frontend)/_components/ImageGallery.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { Media } from '@/payload-types'
 import Image from 'next/image'
 import Link from 'next/link'
--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './../../styles/globals.css'
-import Link from 'next/link'
 import Navbar from './_components/Navbar'
 import Footer from './_components/Footer'
 
